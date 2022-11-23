d2a8be17e5189551bc916c1d0883c841ff9544c9
Diana <dianavw8@gmail.com>
2022-11-22 19:09:47 -0500

    Fetch & Vars for current weather, fixed min max temp alg.

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,6 @@ function handleShowMeButtonClick (event) {
     var day5Date;
     var day5HT;
     var day5LT;
-    //currentweather variables
 
     //forecast data fetch
     fetch(forecastWeatherApiUrl).then((response) => response.json()).then((data) => 
@@ -67,71 +66,59 @@ function handleShowMeButtonClick (event) {
         cityName = data.city.name;
 
         var forecastList = data.list;
+        day1Date = dayjs().add(1, 'day').format('YYYY-MM-DD');
+        day2Date = dayjs().add(2, 'day').format('YYYY-MM-DD');
+        day3Date = dayjs().add(3, 'day').format('YYYY-MM-DD');
+        day4Date = dayjs().add(4, 'day').format('YYYY-MM-DD');
+        day5Date = dayjs().add(5, 'day').format('YYYY-MM-DD');
 
         for (var i = 0; i < forecastList.length; i++)
         {
-            if(i <= 5)
+            if(day1Date == forecastList[i].dt_txt.slice(0, 10))
             {
-                day1Date = forecastList[i].dt_txt.slice(0, 10);
-
                 if(day1HT < forecastList[i].main.temp_max || day1HT == undefined){
                     day1HT =  forecastList[i].main.temp_max;
                 }
                 if(day1LT > forecastList[i].main.temp_min || day1LT == undefined){
                     day1LT =  forecastList[i].main.temp_min;
                 }
-
             }
-            if(i >= 6 && i <= 13)
+            if(day2Date == forecastList[i].dt_txt.slice(0, 10))
             {
-                day2Date = forecastList[i].dt_txt.slice(0, 10);
-
                 if(day2HT < forecastList[i].main.temp_max || day2HT == undefined){
                     day2HT =  forecastList[i].main.temp_max;
                 }
                 if(day2LT > forecastList[i].main.temp_min || day2LT == undefined){
                     day2LT =  forecastList[i].main.temp_min;
                 }
-
             }
-            if(i >= 14 && i <= 21)
+            if(day3Date == forecastList[i].dt_txt.slice(0, 10))
             {
-                day3Date = forecastList[i].dt_txt.slice(0, 10);
-
                 if(day3HT < forecastList[i].main.temp_max || day3HT == undefined){
                     day3HT =  forecastList[i].main.temp_max;
                 }
                 if(day3LT > forecastList[i].main.temp_min || day3LT == undefined){
                     day3LT =  forecastList[i].main.temp_min;
                 }
-
             }
-            if(i >= 22 && i <= 29)
+            if(day4Date == forecastList[i].dt_txt.slice(0, 10))
             {
-                day4Date = forecastList[i].dt_txt.slice(0, 10);
-
                 if(day4HT < forecastList[i].main.temp_max || day4HT == undefined){
                     day4HT =  forecastList[i].main.temp_max;
                 }
                 if(day4LT > forecastList[i].main.temp_min || day4LT == undefined){
                     day4LT =  forecastList[i].main.temp_min;
                 }
-
             }
-            if(i >= 30&& i <= 37)
+            if(day5Date == forecastList[i].dt_txt.slice(0, 10))
             {
-                day5Date = forecastList[i].dt_txt.slice(0, 10);
-
                 if(day5HT < forecastList[i].main.temp_max || day5HT == undefined){
                     day5HT =  forecastList[i].main.temp_max;
                 }
                 if(day5LT > forecastList[i].main.temp_min || day5LT == undefined){
                     day5LT =  forecastList[i].main.temp_min;
                 }
-
             }
-
-
         }
 
         console.log('day1LT', day1LT);
@@ -154,7 +141,6 @@ function handleShowMeButtonClick (event) {
         console.log('day5HT', day5HT);
         console.log('day5Date', day5Date);
 
-
         //element.text.value = day5ht;
 
     });
@@ -163,12 +149,26 @@ function handleShowMeButtonClick (event) {
     fetch(currentWeatherApiUrl).then((response) => response.json()).then((data) => 
     {
         console.log(data);
+        var currentDate = dayjs();
+        var currentTemp = data.main.temp;
+        var currentTempFeels = data.main.feels_like;
+        var currentTempMin = data.main.temp_min;
+        var currentTempMax = data.main.temp_max;
+        var weatherDescription = data.weather[0].description;
+        var weatherIcon = data.weather[0].icon;
+        var weatherIconURL = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
 
-
+        console.log("currentDate:", currentDate);
+        console.log("currentTemp:", currentTemp);
+        console.log("currentTempFeels:", currentTempFeels);
+        console.log("currentTempMin:", currentTempMin);
+        console.log("currentTempMax:", currentTempMax);
+        console.log("weatherDescription:", weatherDescription);
+        console.log("weatherIcon:", weatherIcon);
+        console.log("weatherIconURL:", weatherIconURL);
+        
     });
-
 
 }
 
 showMeButton.addEventListener("click", handleShowMeButtonClick);
-
